55bb4066bd85b5cd567a0f31fbfe59a25ba612b5
dtdjv <98287907+dtdjv@users.noreply.github.com>
2022-09-03 16:12:11 +0200

    start

---
+++ script.js
@@ -0,0 +1,20 @@
+function handleSomeDiv(someDiv) { 
+    console.log("div was handled");
+    
+}
+
+const observer = new MutationObserver(function (mutations, mutationInstance) {
+    const someDiv = document.evaluate("//span[text()='Proponowana dla Ciebie']/../../../../../..", document, null, XPathResult.ANY_TYPE, null);
+    if (someDiv) {
+        handleSomeDiv(someDiv);
+        someDiv.remove
+    }
+});
+
+
+observer.observe(document, {
+    childList: true,
+    subtree:   true
+});
+
+//<a target="_blank" href="https://icons8.com/icon/13716/news-feed">News Feed</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
