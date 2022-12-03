691efa0ddbcc1a09483fe5278ad7b9258864d4dd
dtdjv <98287907+dtdjv@users.noreply.github.com>
2022-12-03 11:43:35 +0100

    Update readme.md

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# GOAL
+Plugin has been created to remove one of the most annoying section on Facebook news feed.
+
+# INSTALLATION
 1. Works only with Chrome browser
 2. Go to Settings -> Addons -> Activate Developer mode (top right corner)
 3. Load extension
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const textToFind = 'Proponowana dla Ciebie'
 
 const observer = new MutationObserver(function (mutations, mutationInstance) {
-        const someDiv = document.evaluate(`//span[text()=${textToFind}]/../../../../../..`, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null).singleNodeValue;
+        const someDiv = document.evaluate(`//span[text()='${textToFind}']/../../../../../..`, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null).singleNodeValue;
         if (someDiv) {
             someDiv.remove()
         }
