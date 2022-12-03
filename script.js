a40a9a2f3a01e8d004b94343af734ca6fe58539e
dtdjv <98287907+dtdjv@users.noreply.github.com>
2022-12-03 11:38:07 +0100

    Moving text to find to variable

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+const textToFind = 'Proponowana dla Ciebie'
+
 const observer = new MutationObserver(function (mutations, mutationInstance) {
-        const someDiv = document.evaluate("//span[text()='Proponowana dla Ciebie']/../../../../../..", document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null).singleNodeValue;
+        const someDiv = document.evaluate(`//span[text()=${textToFind}]/../../../../../..`, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null).singleNodeValue;
         if (someDiv) {
             someDiv.remove()
         }
