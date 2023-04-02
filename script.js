42c8035a4e19eaaecbdc31d4d388bb93dc42c1be
dtdjv <98287907+dtdjv@users.noreply.github.com>
2023-04-02 19:03:04 +0200

    new features

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-const textToFind = 'Proponowana dla Ciebie'
+const forYou = 'Proponowana dla Ciebie'
+const events = 'Proponowane wydarzenia'
 
 const observer = new MutationObserver(function (mutations, mutationInstance) {
-        const someDiv = document.evaluate(`//span[text()='${textToFind}']/../../../../../..`, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null).singleNodeValue;
-        if (someDiv) {
-            someDiv.remove()
+
+        const forYouSection = document.evaluate(`//span[text()='${forYou}']/../../../../../..`, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null).singleNodeValue
+        if (forYouSection) {
+            forYouSection.remove()
+        }
+
+        const eventsSection = document.evaluate(`//span[text()='${events}']/../../../../../..`, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null).singleNodeValue
+        if (eventsSection) {
+            eventsSection.remove()
+        }
+
+        const sponsored = document.evaluate(`//a[contains(@href,'/ads/about/?')]/../../../../../../../../../..`, document, null, XPathResult.ANY_UNORDERED_NODE_TYPE, null).singleNodeValue
+        if (sponsored) {
+            sponsored.remove()
         }
 })    
     
